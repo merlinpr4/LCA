eaa395d35c796874a65a95228ae8a9e9b2993dfa
Merlin <prasadme@tcd.ie>
2021-09-30 16:38:39 +0100

    created unit tests for LCA implementation of Java using junit tests. Achieved 100% code coverage and passed all tests succefully. Also removed main from LCA.java.

--- LCA.java
+++ LCA.java
@@ -69,24 +69,4 @@ public class LCA {
 		path.remove(path.size()-1);
 		return false;
 	}
-
-	public static void main(String[] args) {
-
-		LCA bt = new LCA();
-		bt.root = new Node(1);
-		bt.root.right = new Node(2);
-		bt.root.left = new Node(3);
-		bt.root.right.right = new Node(4);
-		bt.root.right.left = new Node(5);
-		bt.root.left.right = new Node(6);
-		bt.root.left.left = new Node(7);
-
-		System.out.println("LCA of 4 and 5 is " + bt.findLCA(4,5));
-		System.out.println("LCA of 6 and 7 is " + bt.findLCA(6,7));
-		System.out.println("LCA of 2 and 4 is " + bt.findLCA(2,4));
-		System.out.println("LCA of 7 and 5 is " + bt.findLCA(7,5));
-		System.out.println("LCA of 7 and 9 is " + bt.findLCA(7,9));
-		
-
-	}
 }
