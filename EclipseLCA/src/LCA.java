b15853692149df2c7b1a3836096d0415e263b126
Merlin <prasadme@tcd.ie>
2021-10-03 01:50:23 +0100

    reorganised LCA java files and added directory from eclipse workspace so bin and .settings folders are included

---
+++ EclipseLCA/src/LCA.java
@@ -0,0 +1,72 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+	int data;
+	Node left, right;
+
+	Node(int value) {
+		data = value;
+		left = right = null;
+	}
+}
+
+public class LCA {
+
+	Node root;
+	private List<Integer> path1 = new ArrayList<>();
+	private List<Integer> path2 = new ArrayList<>();
+
+	int findLCA(int a, int b) {
+		path1.clear();
+		path2.clear();
+		return findLowestCommonAncestor(root, a, b);
+	}
+
+	private int findLowestCommonAncestor(Node root, int a, int b) {
+		int i;
+		if (!findPath(root, a, path1) || !findPath(root, b, path2)) {
+
+			if(path1.size()==0)
+			{
+				System.out.println("First number is missing");
+			}
+			else if (path2.size()== 0)
+			{
+				System.out.println("Second number is missing");
+			}
+			return -1;
+		}
+
+		for (i = 0; i < path1.size() && i < path2.size(); i++) {
+			if (!path1.get(i).equals(path2.get(i)))
+				return path1.get(i-1);
+		}
+		return path1.get(i-1);
+	}
+
+	private boolean findPath(Node root, int val, List<Integer> path)
+	{
+		if (root == null) {
+			return false;
+		}
+
+		path.add(root.data);
+
+		if (root.data == val) {
+			return true;
+		}
+
+		if (root.left != null && findPath(root.left, val, path)) {
+			return true;
+		}
+
+		if (root.right != null && findPath(root.right, val, path)) {
+			return true;
+		}
+
+		path.remove(path.size()-1);
+		return false;
+	}
+}
