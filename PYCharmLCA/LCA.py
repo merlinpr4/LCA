ba11a7e8be52b5aa5cf72d8acbc78023623af95a
Merlin <prasadme@tcd.ie>
2021-10-03 02:28:40 +0100

    Removed testing from main for LCA.py

--- PYCharmLCA/LCA.py
+++ PYCharmLCA/LCA.py
@@ -33,11 +33,4 @@ def findPath(root, val, path):
     path.pop()
     return False
 
-root = Node(1)
-root.right = Node(2)
-root.left = Node(3)
-root.right.right = Node(4)
-root.right.left = Node(5)
-root.left.right = Node(6)
-root.left.left = Node(7)
 
