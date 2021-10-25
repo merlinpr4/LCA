255103591ce4ebe1598bb72b5a3681a55d653045
Merlin <prasadme@tcd.ie>
2021-10-26 00:13:45 +0100

    Used github to find old versions of LCA and LCATest as I want to retest merging as that was the main point of this excercise

--- LCATest.java
+++ LCATest.java
@@ -2,7 +2,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.junit.Test;
 @RunWith(JUnit4.class)
 public class LCATest {
 	@Test
@@ -15,7 +14,7 @@ public class LCATest {
 		bt.root.right.left = new Node(5);
 		bt.root.left.right = new Node(6);
 		bt.root.left.left = new Node(7);
-		
+
 		assertEquals("LCA of 4 and 5 is 2",2, bt.findLCA(4,5));
 		assertEquals("LCA of 6 and 7 is 3",3, bt.findLCA(6,7));
 		assertEquals("LCA of 2 and 4 is 2",2, bt.findLCA(2,4));
@@ -34,10 +33,27 @@ public class LCATest {
 		 * */
 
 	}
+	
+	@Test
+	public void testNonExistentNode() {
+		LCA bt = new LCA();
+		bt.root = new Node(1);
+		bt.root.right = new Node(2);
+		bt.root.left = new Node(3);
 
-@@ -47,4 +59,130 @@ public void testNonExistentTree() {
+		assertEquals("LCA of 1 and 5 is -1 (not existent)",-1, bt.findLCA(1,5));
+		assertEquals("LCA of 5 and 2 is -1 (not existent)",-1, bt.findLCA(5,2));
+	}
+	
+	@Test
+	public void testNonExistentTree() {
+		LCA bt = new LCA();
+
+		assertEquals("LCA of 2 and 3 is -1 (not existent)",-1, bt.findLCA(2,3));
+
 		assertEquals("LCA of 5 and 1 is -1 (not existent)",-1, bt.findLCA(5,1));
 	}
+
 
 	@Test
 	public void testLCASmallTree() {
